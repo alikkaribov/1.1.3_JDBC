6e275a8a458336fe12a5b5190f05620e81b31d7e
Alik <81850964+alikkaribov@users.noreply.github.com>
2022-10-11 23:44:56 +0400

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -0,0 +1,97 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public class UserDaoHibernateImpl implements UserDao {
+    public UserDaoHibernateImpl() {
+    }
+    private SessionFactory factory = new Util().getInstance().getSessionFactory();
+    @Override
+    public void createUsersTable() {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS USER"
+                    + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+                    + "name VARCHAR(45) NOT NULL,"
+                    + "lastName VARCHAR(45) NOT NULL,"
+                    + "age INT NOT NULL)").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица создана!");
+        } catch (RuntimeException e) {
+            System.out.println("Таблица НЕ создана!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void dropUsersTable() {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS USER").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Таблица удалена!");
+        } catch (RuntimeException e) {
+            System.out.println("Таблица НЕ удалена!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void saveUser(String name, String lastName, byte age) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(new User(name, lastName, age));
+            session.getTransaction().commit();
+            System.out.println("User с именем – " + name + " добавлен в базу данных.");
+        } catch (RuntimeException e) {
+            System.out.println("User " + name + " " + lastName + " НЕ может быть добален в таблицу!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeUserById(long id) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            User user = session.load(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            session.getTransaction().commit();
+            System.out.println("User с ID = " + id + " удален.");
+        } catch (RuntimeException e) {
+            System.out.println("User с ID = " + id + " НЕ может быть удален!");
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        List listOfUsers = null;
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            listOfUsers = session.createQuery("From User").list();
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("Ошибка вывода списка пользователей");
+        }
+        return listOfUsers;
+    }
+    @Override
+    public void cleanUsersTable() {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.createSQLQuery("TRUNCATE TABLE USER").executeUpdate();
+            session.getTransaction().commit();
+            System.out.println("Записи пользователей удалены в таблица User!");
+        } catch (RuntimeException e) {
+            System.out.println("Таблица не может быть очищена!");
+            e.printStackTrace();
+        }
+    }
+}
---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,103 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+    }
+    public void createUsersTable() {
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE TABLE IF NOT EXISTS USER"
+                    + "(id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,"
+                    + "name VARCHAR(45) NOT NULL,"
+                    + "lastName VARCHAR(45) NOT NULL,"
+                    + "age INT NOT NULL)");
+            System.out.println("Таблица создана!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Таблица НЕ создана!");
+        }
+    }
+
+    public void dropUsersTable() {
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("DROP TABLE IF EXISTS USER");
+            System.out.println("Таблица удалена!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Таблица НЕ удалена!");
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        try(
+                Connection connection = Util.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "INSERT INTO USER (NAME, LASTNAME, AGE) VALUES(?, ?, ?)");) {
+
+            preparedStatement.setString(1, name);
+            preparedStatement.setString(2, lastName);
+            preparedStatement.setByte(3, age);
+            preparedStatement.executeUpdate();
+            System.out.println("User с именем - " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("User " + name + " " + lastName + " не может быть добален в таблицу!");
+        }
+    }
+
+    public void removeUserById(long id) {
+        try(
+                Connection connection = Util.getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(
+                        "DELETE FROM USER WHERE ID=?")) {
+
+            preparedStatement.setLong(1, id);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("User с ID = " + id + " не может быть удален!");
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> listOfUsers = new ArrayList<>();
+        try(
+                Connection connection = Util.getConnection(); Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM USER");) {
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("ID"));
+                user.setName(resultSet.getString("NAME"));
+                user.setLastName(resultSet.getString("LASTNAME"));
+                user.setAge(resultSet.getByte("AGE"));
+                listOfUsers.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Ошибка чтения из таблицы!");
+        }
+        return listOfUsers;
+    }
+
+    public void cleanUsersTable() {
+        try(Connection connection = Util.getConnection(); Statement statement = connection.createStatement()) {
+            statement.executeUpdate("TRUNCATE TABLE USER");
+            System.out.println("Записи пользователей удалены в таблица User!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Таблица не может быть очищена!");
+        }
+    }
+}
+
